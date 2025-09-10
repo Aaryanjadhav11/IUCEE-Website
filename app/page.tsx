b1c9500f5371d025c3229bef198a609f6ab4dde0
Aaryanjadhav11 <aaryanjadhav9007@gmail.com>
2025-09-10 15:40:59 +0530

    Ready for Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation';
 
 export default function Page() {
     // Serve the static site from /site/index.html
-    redirect('/site/index.html');
+    redirect('index.html');
 }
